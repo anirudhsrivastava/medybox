e8d34807897b97cbf8512b46763ecc682ac1c078
Anirudh Srivastava <anirudh.srivastava@gmail.com>
2015-02-15 19:54:08 +0530

    Adds user firsts specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#RSpec.describe User, type: :model do
+describe User do
+	before { @user = FactoryGirl.build(:user) }
+
+	subject { @user }
+
+	it { should respond_to(:email) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+
+	it { should be_valid }
+   #pending "add some examples to (or delete) #{__FILE__}"
 end
