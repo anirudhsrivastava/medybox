3833fa95efb86a1ff80e78c2ebe7b3ef254fe9ab
Anirudh Srivastava <anirudh.srivastava@gmail.com>
2015-03-02 14:50:25 +0530

    Adds update and destroy action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   #                            constraints: { subdomain: 'api' }, path: '/'  do
    namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
-  #            constraints: ApiConstraints.new(version: 1, default: true)  do
+         # constraints: ApiConstraints.new(version: 1, default: true)  do
          # We are going to list our resources here
-         resources :users, :only => [:show, :create]
+         resources :users, :only => [:show, :create, :update, :destroy]
     end
   end 
 end
